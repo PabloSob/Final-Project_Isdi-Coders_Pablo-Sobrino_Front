9dc324123fab1c48bb064ff0ab0e29f79fdbaa91
Pablo S <sobrinopablo@gmail.com>
2022-08-31 09:39:49 +0200

    style colors file creation

---
+++ src/styleMainTheme.tsx
@@ -0,0 +1,17 @@
+const styledMainTheme = {
+  textFirstColor: "#FFFFFF",
+  textSecondColor: "#000000",
+  textThirdColor: "#159A9C",
+  textEnterInputColor: "#9E9E9E",
+  backgroundFirstColor: "#DEEFE7",
+  backgroundShadow: "#002333",
+  loadingColor: "#159A9C",
+  errorColor: "#FF0000",
+  successColor: "#55B938",
+  borderFirstColor: "#159A9C",
+  borderSecondColor: "#1D4C4F",
+  primaryFont: "'Roboto', sans-serif",
+  brandLogoFont: "'Arial', sans-serif",
+};
+
+export default styledMainTheme;
