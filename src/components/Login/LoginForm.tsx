89939227bc57a2737e05a18d3c21ebd196688449
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:02:22 +0200

    component login form creation

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -2,10 +2,35 @@ import LoginStyled from "./LoginFormStyled";
 import Button from "../Button/Button";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import useUserApi from "../../hooks/useUserApi";
+import { SyntheticEvent, useState } from "react";
 
 const LoginForm = (): JSX.Element => {
+  const initialState = {
+    userName: "",
+    password: "",
+  };
+
+  const { login } = useUserApi();
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmitData = (event: SyntheticEvent) => {
+    event.preventDefault();
+    login({
+      userName: formData.userName,
+      password: formData.password,
+    });
+  };
+
+  const onChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  const hasEmptyFields =
+    formData.userName.length < 5 || formData.password.length < 5;
+
   return (
-    <LoginStyled className="form" noValidate onSubmit={() => {}}>
+    <LoginStyled className="form" onSubmit={onSubmitData}>
       <ToastContainer />
       <div className="logo__container">
         <img
@@ -16,48 +41,47 @@ const LoginForm = (): JSX.Element => {
         <span className="logo__text">Crypto Realm</span>
       </div>
       <h2 className="login-form__form-title">Hi, welcome back!</h2>
-      <div className="login-form__container">
-        <div className="login-form__input__container">
-          <label htmlFor="username" className="input__title">
-            Username
-          </label>
-          <input
-            className="login-form__input"
-            placeholder="Enter your username"
-            onChange={() => {}}
-            value={""}
-            type="text"
-            id="username"
-            autoComplete="off"
-            required
-          ></input>
+      <form action="" className="login-form" noValidate>
+        <div className="login-form__container">
+          <div className="login-form__input__container">
+            <label htmlFor="userName" className="input__title">
+              Username
+            </label>
+            <input
+              className="login-form__input"
+              placeholder="Enter your username"
+              onChange={onChangeData}
+              value={formData.userName}
+              type="text"
+              id="userName"
+              autoComplete="off"
+              required
+            ></input>
+          </div>
+          <div className="login-form__input__container">
+            <label htmlFor="password" className="form__label">
+              Password
+            </label>
+            <input
+              className="login-form__input"
+              value={formData.password}
+              onChange={onChangeData}
+              required
+              type="password"
+              id="password"
+              autoComplete="off"
+              placeholder="Enter your password"
+            ></input>
+          </div>
         </div>
-        <div className="login-form__input__container">
-          <label htmlFor="password" className={`input__title ${""}`}>
-            Password
-          </label>
-          <input
-            className="login-form__input"
-            value={""}
-            onChange={() => {}}
-            required
-            type="password"
-            id="password"
-            autoComplete="off"
-            placeholder="Enter your password"
-          ></input>
-        </div>
-      </div>
-      <Button
-        buttonText="Login"
-        type="submit"
-        classNameTypeButton="button--big"
-        actionOnclick={() => {}}
-      />
-      <div className="login-form__link-login">
-        <span>Don't have an account?</span>
-        <a href="/register">Login</a>
-      </div>
+        <Button
+          buttonText="Login"
+          type="submit"
+          classNameTypeButton="button--big"
+          actionOnclick={() => {}}
+          isDisable={hasEmptyFields}
+        />
+      </form>{" "}
     </LoginStyled>
   );
 };
