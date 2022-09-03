a8a21c0d85e2477168e52bf80bc92a760d768264
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:10:21 +0200

    userSlice creation

---
+++ src/store/users/slices/userSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../../interfaces/userInterfaces";
+
+const userInitialState: User = {
+  id: "",
+  token: "",
+  userName: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: userInitialState,
+  reducers: {
+    loginUser: (previousUsers, action: PayloadAction<User>) => action.payload,
+  },
+});
+
+export const { reducer: userReducer } = userSlice;
+
+export const { loginUser: loginUsersActionCreator } = userSlice.actions;
+
+export default userSlice.reducer;
