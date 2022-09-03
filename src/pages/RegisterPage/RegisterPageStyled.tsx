e54bc444cf20e376d66da089d749ce4957f9fb81
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:22:48 +0200

    added style and tested RegisterPage

---
+++ src/pages/RegisterPage/RegisterPageStyled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LinkStyledRegister = styled.div`
+  .register-text__text {
+    margin-left: 15px;
+    color: #9e9e9e;
+    display: flex;
+    align-items: center;
+    gap: 5rem;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+`;
+
+export default LinkStyledRegister;
