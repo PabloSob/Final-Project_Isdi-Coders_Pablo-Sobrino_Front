c636ab3530fe1a2febd344715b191a0c95d6d788
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:10:39 +0200

    userSlice test

---
+++ src/store/users/slices/userSlice.test.tsx
@@ -0,0 +1,26 @@
+import { User } from "../../interfaces/userInterfaces";
+import usersSlice, { loginUsersActionCreator } from "./userSlice";
+
+describe("Given a user slice", () => {
+  describe("When invoked with an initial state as previous user and a login user action creator with a fakeUserTest", () => {
+    test("Then it should return the fakeUser", () => {
+      const initialState: User = {
+        id: "",
+        token: "",
+        userName: "",
+      };
+      const fakeUserTest: User = {
+        id: "000001tt",
+        token: "asdf234",
+        userName: "Vivaldi",
+      };
+
+      const user = usersSlice(
+        initialState,
+        loginUsersActionCreator(fakeUserTest)
+      );
+
+      expect(user).toStrictEqual(fakeUserTest);
+    });
+  });
+});
