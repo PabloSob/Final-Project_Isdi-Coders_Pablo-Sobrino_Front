84e92404e97262de26ace45fad9449b3ac27c9ea
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:23:14 +0200

    creation LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { ButtonStyled } from "../../components/Button/ButtonStyled";
 import LoginForm from "../../components/Login/LoginForm";
+import LinkStyledLogin from "./LoginPageStyled";
 
 const LoginPage = (): JSX.Element => {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+      <LinkStyledLogin className="register-text">
+        <span className="register-text__text">
+          Don't have an account?
+          <Link to={"/register"}>
+            <ButtonStyled className="button-link">Sign up</ButtonStyled>
+          </Link>
+        </span>
+      </LinkStyledLogin>
+    </>
+  );
 };
 export default LoginPage;
