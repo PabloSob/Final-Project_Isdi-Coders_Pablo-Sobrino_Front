f165b4975df9ba3019f5e448defe4df40a563044
Pablo S <sobrinopablo@gmail.com>
2022-09-03 12:22:26 +0200

    updated RegisterPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { ButtonStyled } from "../../components/Button/ButtonStyled";
 import RegisterForm from "../../components/Register/RegisterForm";
+import LinkStyledRegister from "./RegisterPageStyled";
 
 const RegisterPage = (): JSX.Element => {
-  return <RegisterForm />;
+  return (
+    <>
+      <RegisterForm />
+      <LinkStyledRegister className="register-text">
+        <span className="register-text__text">
+          Already have an account?
+          <Link to={"/login"}>
+            <ButtonStyled className="button-link">Login</ButtonStyled>
+          </Link>
+        </span>
+      </LinkStyledRegister>
+    </>
+  );
 };
 export default RegisterPage;
