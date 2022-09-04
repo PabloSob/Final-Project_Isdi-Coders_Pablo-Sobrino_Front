e91c725261c0a3df15187f8627758b4675accf43
Pablo S <sobrinopablo@gmail.com>
2022-09-04 21:58:29 +0200

    included CryptoListPage parth in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import CryptoListPage from "./pages/CryptoListPage/CryptoListPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/crypto" element={<CryptoListPage />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
