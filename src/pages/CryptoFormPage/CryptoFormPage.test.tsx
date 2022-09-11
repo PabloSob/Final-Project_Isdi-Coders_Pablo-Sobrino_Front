f1fb7a8064ed31e1c8770e6d54c0f8c98b050645
Pablo S <sobrinopablo@gmail.com>
2022-09-11 15:50:57 +0200

    test-CryptoFormPage

---
+++ src/pages/CryptoFormPage/CryptoFormPage.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../store/store";
+import CryptoFormPage from "./CryptoFormPage";
+
+const mockUseParams = jest.fn();
+const mockUseAppSelector = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => mockUseParams(),
+}));
+
+jest.mock("../../store/hooks", () => ({
+  ...jest.requireActual("../../store/hooks"),
+  useAppSelector: () => mockUseAppSelector(),
+}));
+
+describe("Given a CryptoFormPage component", () => {
+  describe("When not receives an id", () => {
+    test("Then it should render the created component", () => {
+      mockUseParams.mockReturnValue({ id: undefined });
+      mockUseAppSelector.mockReturnValue([]);
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CryptoFormPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const expectedTextButton = "Create";
+      const button = screen.getByRole("button", {
+        name: expectedTextButton,
+      });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
